704e6ee81cc1fbd8a4a382a7c05b24c55c4133ab
TOP96 <52368134+kanchanok007@users.noreply.github.com>
2022-07-13 10:29:25 +0900

    Working prototype, refactoring needed

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require("express");
+// const socket = require("socket.io");
+const http = require('http').createServer();
+
+const io = require('socket.io')(http, {
+    cors: { origin: "*" }
+});
+
+const app = express();
+app.use(express.static("public"));
+// let server = 
+
+app.get("/", function(req,res){
+    res.sendFile(__dirname+"/index.html");
+});
+app.listen(3000);
+
+io.on("connection", function(socket){
+    console.log("Connect with ", socket.id);
+
+    socket.on("chat",function(data){
+        console.log(data);
+        io.emit("chat", data );  
+    });
+});
+
+http.listen(process.env.PORT || 8080, () => console.log('listening on http://localhost:8080') );
